ff31a34a739ed4fd2ffb06d13a6bc2f44c2ebd7c
Viktoriia Colozzo <viktoriiacolozzo@gmail.com>
2021-04-28 18:28:59 -0400

    These are my changes.

--- src/com/vc/Main.java
+++ src/com/vc/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello from GitHub!");
+        var person = new Person();
 
     }
 }
---
+++ src/com/vc/Person.java
@@ -0,0 +1,4 @@
+package com.vc;
+
+public class Person {
+}
